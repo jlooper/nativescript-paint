6bb14e7a92c90ff6faa05cda7cb284f522221aa3
Nathan Walker <walkerrunpdx@gmail.com>
2019-02-04 16:13:24 -0800

    fix: ios implementation and demo

--- src/paint.common.ts
+++ src/paint.common.ts
@@ -1,33 +1,37 @@
 import { View, Property } from 'tns-core-modules/ui/core/view';
 import { Color } from 'tns-core-modules/color';
-import { PaintPad as PaintPadDefinition } from '.';
 
-export abstract class PaintPadBase extends View implements PaintPadDefinition {
-	public drawColor: Color;
-	public drawWidth: number;
-	public drawOpacity: number;
+export class PaintPadBase extends View {
 
-	public abstract clearPainting(): void;
-	public abstract getPainting(): Promise<any>;
 }
 
-export const drawColorProperty = new Property<PaintPadBase, Color>({
-	name: 'drawColor',
-	valueConverter: v => new Color(v),
-	equalityComparer: Color.equals,
+export const drawColorProperty = new Property<PaintPadBase, string>({
+  name: 'drawColor'
 });
 drawColorProperty.register(PaintPadBase);
 
 export const drawWidthProperty = new Property<PaintPadBase, number>({
-	name: 'drawWidth',
-	defaultValue: 1,
-	valueConverter: v => +v,
+  name: 'drawWidth',
+  defaultValue: 1,
+  valueConverter: v => +v,
 });
 drawWidthProperty.register(PaintPadBase);
 
 export const drawOpacityProperty = new Property<PaintPadBase, number>({
-	name: 'drawOpacity',
-	defaultValue: 1,
-	valueConverter: v => +v,
+  name: 'drawOpacity',
+  defaultValue: 1,
+  valueConverter: v => +v,
 });
 drawOpacityProperty.register(PaintPadBase);
+
+export const airBrushFlowProperty = new Property<PaintPadBase, number>({
+  name: 'airBrushFlow',
+  defaultValue: .7,
+  valueConverter: v => +v,
+});
+airBrushFlowProperty.register(PaintPadBase);
+
+export const canvasColorProperty = new Property<PaintPadBase, string>({
+  name: 'canvasColor'
+});
+canvasColorProperty.register(PaintPadBase);
--- src/paint.ios.ts
+++ src/paint.ios.ts
@@ -1,90 +1,88 @@
-/// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
-/// <reference path="./typings/DAScratchPad.d.ts" />
-
 import { Color } from 'tns-core-modules/color';
-import { PaintPadBase, drawColorProperty, drawWidthProperty, drawOpacityProperty } from './paint.common';
+import { PaintPadBase, drawColorProperty, drawWidthProperty, drawOpacityProperty, canvasColorProperty, airBrushFlowProperty } from './paint.common';
+import { fromNativeSource } from 'tns-core-modules/image-source';
 
 export class PaintPad extends PaintPadBase {
-	public nativeView: DAScratchPadView;
-	/*constructor() {
-		super();
-		this.nativeView = DAScratchPadView.alloc().initWithFrame(CGRectMake(0, 0, 100, 100));
-		this.nativeView.clipsToBounds = true;
-	}*/
-	createNativeView() {
-		const scratchPad = DAScratchPadView.alloc().initWithFrame(CGRectMake(0, 0, 100, 100));
-		scratchPad.clipsToBounds = true;
-		return scratchPad;
-	}
+  public nativeView: DAScratchPadView;
+  private _canvasColor: string;
 
-	initNativeView() {
-		this.nativeView.clipsToBounds = true;
-		super.initNativeView();
-	}
+  createNativeView() {
+    const scratchPad = DAScratchPadView.alloc().initWithFrame(CGRectMake(0, 0, 100, 100));
+    // console.log('scratchPad:', scratchPad);
+    // scratchPad.drawColor = new Color('#3489db').ios;
+    // scratchPad.drawWidth = 3;
+    // scratchPad.drawOpacity = 1;
+    return scratchPad;
+  }
 
-	get ios(): DAScratchPadView {
-		return this.nativeView;
-	}
+  [canvasColorProperty.setNative](value: string) {
+    // console.log('canvasColorProperty:', value);
+    this._canvasColor = value;
+    this.nativeView.clearToColor(new Color(value).ios);
+  }
 
-	[drawWidthProperty.getDefault](): number {
-		console.log(this.nativeView);
-		return this.nativeView.drawWidth;
-	}
+  [drawWidthProperty.getDefault](): number {
+    return this.nativeView.drawWidth;
+  }
 
-	[drawWidthProperty.setNative](value: number) {
-		console.log('drawWidthProperty value:', value);
-		console.log('this.nativeView:', this.nativeView);
-		this.nativeView.drawWidth = value;
-	}
+  [drawWidthProperty.setNative](value: number) {
+    // console.log('drawWidthProperty value:', value);
+    // console.log('this.nativeView:', this.nativeView);
+    this.nativeView.drawWidth = value;
+  }
+  
+  [drawColorProperty.setNative](value: string) {
+    // console.log('drawColorProperty value:', value);
+    // console.log('this.nativeView:', this.nativeView);
+    this.nativeView.drawColor = new Color(value).ios;
+  }
 
-	[drawColorProperty.getDefault](): UIColor {
-		return this.nativeView.drawColor;
-	}
+  [drawColorProperty.getDefault](): UIColor {
+    return this.nativeView.drawColor;
+  }
 
-	[drawOpacityProperty.getDefault](): number {
-		return this.nativeView.drawOpacity;
-	}
+  [drawOpacityProperty.getDefault](): number {
+    return this.nativeView.drawOpacity;
+  }
 
-	/*public onLoaded() {
-		if (this.width) {
-			this.nativeView.frame.size.width = 300;
-		}
-		if (this.height) {
-			this.nativeView.frame.size.height = 300;
-		}
-		super.onLoaded();
-	}*/
+  [airBrushFlowProperty.getDefault](): number {
+    return this.nativeView.airBrushFlow;
+  }
 
-	public getPainting(): Promise<any> {
-		return new Promise((resolve, reject) => {
-			try {
-				const data = this.nativeView.getSketch();
-				if (data) {
-					resolve(data);
-				} else {
-					reject('Pad is empty.');
-				}
-			} catch (err) {
-				reject(err);
-			}
-		});
-	}
+  [airBrushFlowProperty.setNative](value: number) {
+    if (value < 0 || value > 1) {
+      throw new Error('value range can only be 0 - 1');
+    }
+    this.nativeView.airBrushFlow = value;
+  }
 
-	public clearPainting(): void {
-		try {
-			if (this.backgroundColor) {
-				let color = this.backgroundColor.toString;
-				if (color.constructor == Color) {
-					color = (color as any).ios;
-				} else if (color.constructor == String) {
-					color = new Color(<any>color).ios;
-				}
-				this.nativeView.clearToColor(color as any);
-			} else {
-				//this.nativeView.clearToColor(Color);
-			}
-		} catch (err) {
-			console.log('Error clearing the paint pad: ' + err);
-		}
-	}
+  public getPainting(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      try {
+        const data = this.nativeView.getSketch();
+        if (data) {
+          resolve(fromNativeSource(data));
+        } else {
+          reject('Pad is empty.');
+        }
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  public clearPainting(): void {
+    try {
+      let color;
+      if (this._canvasColor) {
+        color = new Color(this._canvasColor).ios;
+      } else {
+        // default to white
+        color = new Color('#fff').ios;
+      }
+      this.nativeView.clearToColor(color);
+    } catch (err) {
+      console.log('Error clearing the paint pad: ' + err);
+    }
+  }
 }
